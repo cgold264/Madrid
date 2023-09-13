e3fc327a6ebe875df5be74567263072e95828221
cgold264 <cgoldschmidt04@gmail.com>
2023-09-13 09:10:47 +0200

    first

---
+++ src/test/java/com/example/madridbeers/madrid/MadridApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.madridbeers.madrid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MadridApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
